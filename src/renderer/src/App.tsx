f80ca967efda53e0447281f7ff4f2f499279c328
sparkpilot <sparkpilot@ai-copilots.io>
2025-06-25 09:41:01 +0800

    refactor(ui): streamline css and adopt tailwind utility classes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,30 +5,51 @@ function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="py-4 text-center text-3xl font-bold underline">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <img
+        alt="logo"
+        className="mb-5 h-32 w-32 transition-transform duration-300 hover:drop-shadow-[0_0_1.2em_#6988e6aa]"
+        src={electronLogo}
+      />
+      <div className="mb-2.5 font-semibold text-gray-400">Powered by electron-vite</div>
+      <div className="py-4 text-center text-3xl font-bold">
+        Build an Electron app with{' '}
+        <span className="bg-gradient-to-r from-teal-500 to-indigo-500 bg-clip-text font-bold text-transparent">
+          React
+        </span>
+        &nbsp;and{' '}
+        <span className="bg-gradient-to-r from-blue-600 to-yellow-400 bg-clip-text font-bold text-transparent">
+          TypeScript
+        </span>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
+      <p className="text-md font-semibold text-gray-400">
+        Please try pressing <code className="rounded bg-gray-800 p-1 font-mono">F12</code> to open
+        the devTool
       </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+      <div className="m-[-6px] flex flex-wrap justify-start p-8">
+        <div className="flex-shrink-0 p-1.5">
+          <a
+            className="inline-block cursor-pointer whitespace-nowrap rounded-full border border-gray-600 bg-gray-700 px-5 py-2 text-sm font-semibold text-white no-underline hover:border-gray-500 hover:bg-gray-600"
+            href="https://electron-vite.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Documentation
           </a>
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+        <div className="flex-shrink-0 p-1.5">
+          <a
+            className="inline-block cursor-pointer whitespace-nowrap rounded-full border border-gray-600 bg-gray-700 px-5 py-2 text-sm font-semibold text-white no-underline hover:border-gray-500 hover:bg-gray-600"
+            target="_blank"
+            rel="noreferrer"
+            onClick={ipcHandle}
+          >
             Send IPC
           </a>
         </div>
       </div>
       <Versions></Versions>
-    </>
+    </div>
   )
 }
 
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -4,10 +4,10 @@ function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="fixed bottom-5 inline-flex items-center overflow-hidden rounded-full bg-zinc-800 p-4 font-mono text-sm text-opacity-80 backdrop-blur-2xl">
+      <li className="border-r border-gray-500 px-5">Electron v{versions.electron}</li>
+      <li className="border-r border-gray-500 px-5">Chromium v{versions.chrome}</li>
+      <li className="px-5">Node v{versions.node}</li>
     </ul>
   )
 }
